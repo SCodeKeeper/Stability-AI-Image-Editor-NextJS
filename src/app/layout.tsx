8adae80c99e4afdd23f483178116329969fa5771
Yassir Abbassi <y26361442@gmail.com>
2025-06-15 17:53:47 +0100

    v0.3.3 c2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
       { url: '/logo200.png', sizes: '200x200', type: 'image/png' }
     ],
     apple: '/logo200.png',
+    shortcut: '/favicon.ico',
   },
 };
 
@@ -27,6 +28,8 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/logo200.png" type="image/png" />
         <link rel="apple-touch-icon" href="/logo200.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
